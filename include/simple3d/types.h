9152dbc5e8348cfc0f67fe7713a603ad3d80b450
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-11-10 22:57:34 +0300

    Add header with all types

---
+++ include/simple3d/types.h
@@ -0,0 +1,22 @@
+#ifndef SIMPLE3D_TYPES_H
+#define SIMPLE3D_TYPES_H
+
+namespace Simple3D {
+
+
+
+class MainLoop;
+class Window;
+class WindowBuilder;
+class Scene;
+class Drawable;
+enum class ErrorType;
+struct Error;
+class Shader;
+class ShaderBuilder;
+
+
+
+}
+
+#endif
