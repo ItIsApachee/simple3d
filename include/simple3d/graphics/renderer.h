cbf1bdfa6c232a6b53900d46f9abfc6f270bc6a5
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-11-23 16:49:07 +0300

    Add Renderer class

--- include/simple3d/graphics/renderer.h
+++ include/simple3d/graphics/renderer.h
@@ -5,6 +5,17 @@
 #ifndef SIMPLE3D_GRAPHICS_RENDERED_H
 #define SIMPLE3D_GRAPHICS_RENDERED_H
 
-// todo
+namespace Simple3D {
+
+
+
+/** \class Renderer simple3d/graphics/renderer.h
+ * \brief 
+*/
+class Renderer {};
+
+
+
+}
 
 #endif
