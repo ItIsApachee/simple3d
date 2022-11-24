b5703f67d078c0c1d6aaed177fbd90e2bd4eff3f
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-11-24 09:51:51 +0300

    Move renderers to separate folder

--- include/simple3d/graphics/graphics.h
+++ include/simple3d/graphics/graphics.h
@@ -9,7 +9,7 @@
 #define SIMPLE3D_GRAPHICS_GRAPHICS_H
 
 #include <simple3d/types.h>
-#include <simple3d/graphics/renderer.h>
+// #include <simple3d/graphics/renderer.h>
 #include <simple3d/graphics/scene.h>
 #include <simple3d/graphics/drawable.h>
 #include <simple3d/graphics/camera.h>
