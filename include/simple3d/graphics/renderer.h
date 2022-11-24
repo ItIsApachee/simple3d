141786be7f82198de1c45c0ce52b6892eda656a3
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-11-25 01:32:05 +0300

    Add IRenderer interaface

---
+++ include/simple3d/graphics/renderer.h
@@ -0,0 +1,37 @@
+#ifndef SIMPLE3D_GRAPHICS_RENDERER_H
+#define SIMPLE3D_GRAPHICS_RENDERER_H
+
+#include <unordered_map>
+
+#include <simple3d/types.h>
+#include <simple3d/graphics/scene.h>
+
+namespace Simple3D {
+
+
+
+/**
+ * \class IRenderer simple3d/graphics/renderer.h
+ * \brief Interface for renderers.
+*/
+class IRenderer {
+public:
+    IRenderer() = default;
+    virtual ~IRenderer() = default;
+
+    virtual void Draw() = 0;
+    virtual void Destroy(Scene* scene) = 0;
+};
+
+namespace Internal {
+    // should it be a class?
+    template <typename R>
+    std::unordered_map<Scene*, R> RendererStorage{};
+}
+// TODO: add is_renderer trait
+
+
+
+}
+
+#endif
