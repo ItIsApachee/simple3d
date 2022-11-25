bf8d90a017ae4dec3b561e7ea7efe86aba98382c
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-11-25 17:07:27 +0300

    Add flexibility to Scene::Create

--- include/simple3d/graphics/scene.h
+++ include/simple3d/graphics/scene.h
@@ -6,6 +6,8 @@
 #define SIMPLE3D_GRAPHICS_SCENE_H
 
 #include <vector>
+#include <type_traits>
+#include <utility>
 
 #include <simple3d/types.h>
 #include <simple3d/graphics/view.h>
@@ -33,36 +35,96 @@ public:
     // TODO: add ability to initialize renderer like Scene::AddRenerer<R>(R&& renderer)
     // TODO: add ability to have multiple instances of renderers of the same type
     // usage example: group objects in separate renderers with different configuration
+    // template <typename P, typename... Args>
+    // Primitive<P> Create(Args... args);
     template <typename P, typename... Args>
-    Primitive<P> Create(Args... args);
+    auto Create(Args... args)
+        -> std::enable_if_t<
+            std::is_same_v<
+                decltype(std::declval<typename P::Renderer>().template Create<P>(args...)),
+                P*>,
+            Primitive<P>>;
+    template <typename P, typename... Args>
+    auto Create(Args... args)
+        -> std::enable_if_t<
+            std::is_same_v<
+                decltype(std::declval<typename P::Renderer>().Create(args...)),
+                P*>,
+            Primitive<P>>;
     template <typename P, typename R, typename... Args>
-    Primitive<P> Create(Args... args);
+    auto Create(Args... args)
+        -> std::enable_if_t<
+            std::is_same_v<
+                decltype(std::declval<R>().template Create<P>(args...)),
+                P*>,
+            Primitive<P>>;
+    template <typename P, typename R, typename... Args>
+    auto Create(Args... args)
+        -> std::enable_if_t<
+            std::is_same_v<
+                decltype(std::declval<R>().Create(args...)),
+                P*>,
+            Primitive<P>>;
 private:
+    template <typename P, typename R, typename... Args>
+    Primitive<P> CreateInternal(P* (R::*mf)(Args...), Args... args);
+
     std::vector<IRenderer*> renderers_{};
 };
 
 
 
 // implementation
-template<typename P, typename... Args>
-Primitive<P> Scene::Create(Args... args) {
-    using Renderer = typename P::Renderer;
-    auto& storage = Internal::RendererStorage<Renderer>;
-    if (storage.find(this) == storage.end()) {
-        storage.insert({this, Renderer{}});
-        renderers_.push_back(&storage[this]);
-    }
-    return Primitive{storage[this].Create(args...)};
+template <typename P, typename... Args>
+auto Scene::Create(Args... args)
+    -> std::enable_if_t<
+        std::is_same_v<
+            decltype(std::declval<typename P::Renderer>().template Create<P>(args...)),
+            P*>,
+        Primitive<P>>
+{
+    return Scene::Create<P, typename P::Renderer>(args...);
+}
+template <typename P, typename... Args>
+auto Scene::Create(Args... args)
+    -> std::enable_if_t<
+        std::is_same_v<
+            decltype(std::declval<typename P::Renderer>().Create(args...)),
+            P*>,
+        Primitive<P>>
+{
+    return Scene::Create<P, typename P::Renderer>(args...);
 }
 template <typename P, typename R, typename... Args>
-Primitive<P> Scene::Create(Args... args) {
+auto Scene::Create(Args... args)
+    -> std::enable_if_t<
+        std::is_same_v<
+            decltype(std::declval<R>().template Create<P>(args...)),
+            P*>,
+        Primitive<P>>
+{
+    return CreateInternal<P, R>(&R::template Create<P>, args...);
+}
+template <typename P, typename R, typename... Args>
+auto Scene::Create(Args... args)
+    -> std::enable_if_t<
+        std::is_same_v<
+            decltype(std::declval<R>().Create(args...)),
+            P*>,
+        Primitive<P>>
+{
+    return CreateInternal<P, R>(&R::Create, args...);
+}
+
+template <typename P, typename R, typename... Args>
+Primitive<P> Scene::CreateInternal(P* (R::*mf)(Args...), Args... args) {
     using Renderer = R;
     auto& storage = Internal::RendererStorage<Renderer>;
     if (storage.find(this) == storage.end()) {
         storage.insert({this, Renderer{}});
         renderers_.push_back(&storage[this]);
     }
-    return Primitive{storage[this].Create(args...)};
+    return Primitive{(storage[this].*mf)(args...)};
 }
 
 
