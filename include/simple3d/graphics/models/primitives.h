710035155182078bf38e4e683347cf4ee1b7a4b1
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-11-26 13:20:28 +0300

    Add primitives.h

---
+++ include/simple3d/graphics/models/primitives.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE3D_GRAPHICS_MODELS_PRIMITIVES_H
+#define SIMPLE3D_GRAPHICS_MODELS_PRIMITIVES_H
+
+#include <simple3d/types.h>
+#include <simple3d/graphics/models/cuboid.h>
+
+namespace Simple3D {
+
+}
+
+#endif
