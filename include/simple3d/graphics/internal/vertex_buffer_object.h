06f1be61382348bd06780578ca5a6c62b652598b
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-01 16:21:06 +0300

    Declare VBO API

--- include/simple3d/graphics/internal/vertex_buffer_object.h
+++ include/simple3d/graphics/internal/vertex_buffer_object.h
@@ -1,4 +1,41 @@
 #ifndef INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_VERTEX_BUFFER_OBJECT_H_
 #define INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_VERTEX_BUFFER_OBJECT_H_
 
+#include <cstddef>
+
+#include <simple3d/graphics/internal/misc.h>
+
+namespace Simple3D::Internal {
+
+
+
+
+class VertexBufferObject {
+ public:
+  explicit VertexBufferObject(bool generate = false,
+      GLenum usage = kDefaultUsage);
+  VertexBufferObject(std::size_t size, std::byte* data = nullptr,
+      GLenum usage = kDefaultUsage);
+  VertexBufferObject(const VertexBufferObject&);
+  VertexBufferObject(VertexBufferObject&&);
+  VertexBufferObject& operator=(const VertexBufferObject&);
+  VertexBufferObject& operator=(VertexBufferObject&&);
+  ~VertexBufferObject();
+
+  void Bind();
+  static void Unbind();
+
+  GLuint vbo();
+  GLenum usage();
+
+ private:
+  GLuint vbo_{kGlesInvalidBuffer};
+  GLenum usage_{kDefaultUsage};
+  std::size_t size_{0};
+};
+
+
+
+}  // namespace Simple3D::Internal
+
 #endif  // INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_VERTEX_BUFFER_OBJECT_H_
---
+++ src/graphics/internal/vertex_buffer_object.cc
@@ -0,0 +1,11 @@
+#include <simple3d/graphics/internal/vertex_buffer_object.h>
+
+namespace Simple3D::Internal {
+
+
+
+static_assert(false, "implement VertexBufferObject");
+
+
+
+}  // namespace Simple3D::Internal
