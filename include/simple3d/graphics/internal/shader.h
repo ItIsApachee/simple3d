851ed2145ade5283492181abee09a0fbb4375bee
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-01 16:40:50 +0300

    Fix include guard

--- include/simple3d/graphics/internal/shader.h
+++ include/simple3d/graphics/internal/shader.h
@@ -5,8 +5,8 @@
  * \brief Managing shaders.
 */
 
-#ifndef INCLUDE_SIMPLE3D_SHADER_SHADER_H_
-#define INCLUDE_SIMPLE3D_SHADER_SHADER_H_
+#ifndef INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_SHADER_H_
+#define INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_SHADER_H_
 
 #include <string>
 #include <optional>
@@ -84,4 +84,4 @@ class ShaderBuilder {
 
 }  // namespace Simple3D
 
-#endif  // INCLUDE_SIMPLE3D_SHADER_SHADER_H_
+#endif  // INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_SHADER_H_
