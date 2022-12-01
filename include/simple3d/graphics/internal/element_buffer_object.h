a533db1a56288ba9cabc424480d7a0b451bd417e
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-01 21:05:54 +0300

    Add EboBuilder

--- include/simple3d/graphics/internal/element_buffer_object.h
+++ include/simple3d/graphics/internal/element_buffer_object.h
@@ -1,20 +1,44 @@
 #ifndef INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_ELEMENT_BUFFER_OBJECT_H_
 #define INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_ELEMENT_BUFFER_OBJECT_H_
 
-#include <cstdint>
 #include <cstddef>
 
 #include <glad/gles2.h>
 
+#include <simple3d/types.h>
 #include <simple3d/graphics/internal/misc.h>
 
 namespace Simple3D::Internal {
 
 
 
+class ElementBufferObjectBuilder {
+ public:
+  ElementBufferObjectBuilder() = default;
+  ElementBufferObjectBuilder(const ElementBufferObjectBuilder&) = default;
+  ElementBufferObjectBuilder(ElementBufferObjectBuilder&&) = default;
+  ElementBufferObjectBuilder&
+  operator=(const ElementBufferObjectBuilder&) = default;
+  ElementBufferObjectBuilder&
+  operator=(ElementBufferObjectBuilder&&) = default;
+  ~ElementBufferObjectBuilder() = default;
+
+  ElementBufferObjectBuilder& Data(std::size_t size, std::byte* data = nullptr);
+  ElementBufferObjectBuilder& Usage(GLenum usage);
+  ElementBufferObject Build(const VertexArrayObject& vao);
+ private:
+  std::byte* data_{nullptr};
+  std::size_t size_{0};
+  GLenum usage_{kDefaultUsage};
+};
+
+
+
 class ElementBufferObject {
 // TODO(apachee): ability to change usage parameter of glBufferData
  public:
+    friend ElementBufferObjectBuilder;
+
     ElementBufferObject() = default;
 //   explicit ElementBufferObject(bool generate = false,
 //       GLenum usage = kDefaultUsage);
@@ -36,6 +60,8 @@ class ElementBufferObject {
   GLenum usage() const;
 
  private:
+  ElementBufferObject(GLuint ebo, std::size_t size, GLenum usage);
+
   GLuint ebo_{kGlesInvalidBuffer};  // handle for OpenGL buffer
   GLenum usage_{kDefaultUsage};  // usage of the buffer
   std::size_t size_{0};  // capacity of OpenGL buffer
