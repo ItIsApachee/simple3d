c4c4153073ca66357af6b43522a2b66a88b4be0c
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-02 07:27:50 +0300

    Deprecate TestPrimitive

--- src/graphics/model.cc
+++ src/graphics/model.cc
@@ -7,26 +7,26 @@ namespace Simple3D {
 
 
 
-void TestPrimitiveRenderer::Draw() {
-  for (auto& p : primitives_) {
-    std::cout << "it works! (" << p->id << ")" << std::endl;
-  }
-}
+// void TestPrimitiveRenderer::Draw() {
+//   for (auto& p : primitives_) {
+//     std::cout << "it works! (" << p->id << ")" << std::endl;
+//   }
+// }
 
-void TestPrimitiveRenderer::Destroy(Scene* scene) {
-  Internal::RendererStorage<TestPrimitiveRenderer>.erase(scene);
-}
+// void TestPrimitiveRenderer::Destroy(Scene* scene) {
+//   Internal::RendererStorage<TestPrimitiveRenderer>.erase(scene);
+// }
 
-TestPrimitiveRenderer::~TestPrimitiveRenderer() {
-  for (auto& i : primitives_)
-    delete i;
-}
+// TestPrimitiveRenderer::~TestPrimitiveRenderer() {
+//   for (auto& i : primitives_)
+//     delete i;
+// }
 
-TestPrimitive* TestPrimitiveRenderer::Create(int id) {
-  std::cout << "create with int" << std::endl;
-  primitives_.emplace_back(new TestPrimitive{id});
-  return primitives_.back();
-}
+// TestPrimitive* TestPrimitiveRenderer::Create(int id) {
+//   std::cout << "create with int" << std::endl;
+//   primitives_.emplace_back(new TestPrimitive{id});
+//   return primitives_.back();
+// }
 
 
 
