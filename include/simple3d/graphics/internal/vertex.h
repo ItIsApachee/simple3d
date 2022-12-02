1c3e09f53aed316be83ff3a966f0a6b593609eaa
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-02 07:20:39 +0300

    Add Vertex

---
+++ include/simple3d/graphics/internal/vertex.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_VERTEX_H_
+#define INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_VERTEX_H_
+
+#include <glad/gles2.h>
+#include <glm/glm.hpp>
+
+namespace Simple3D::Internal {
+
+
+
+struct Vertex {
+ public:
+  // TODO(apachee): add more customization
+  static void BindAttributes();
+
+  glm::vec3 position{0.f, 0.f, 0.f};
+  glm::vec3 normal{1.f, 0.f, 0.f};
+  glm::vec2 texture_coords{0.f, 0.f};
+};
+
+
+
+}
+
+#endif  // INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_VERTEX_H_
---
+++ src/graphics/internal/vertex.cc
@@ -0,0 +1,28 @@
+#include <simple3d/graphics/internal/vertex.h>
+
+#include <glad/gles2.h>
+
+namespace Simple3D::Internal {
+
+
+
+void Vertex::BindAttributes() {
+  glVertexAttribPointer(
+      0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+      reinterpret_cast<void*>(offsetof(Vertex, position)));
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(
+      1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+      reinterpret_cast<void*>(offsetof(Vertex, normal)));
+  glEnableVertexAttribArray(1);
+
+  glVertexAttribPointer(
+      2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+      reinterpret_cast<void*>(offsetof(Vertex, texture_coords)));
+  glEnableVertexAttribArray(2);
+}
+
+
+
+}  // namespace Simple3D::Internal
