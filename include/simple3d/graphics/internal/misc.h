88ed89178f92e777aa657e7a4cca4c22ad3e04c0
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-02 07:23:39 +0300

    Add blank lines for better readability

--- include/simple3d/graphics/internal/misc.h
+++ include/simple3d/graphics/internal/misc.h
@@ -5,6 +5,9 @@
 #include <glad/gles2.h>
 
 namespace Simple3D::Internal {
+
+
+
 constexpr GLuint kGlesInvalidBuffer = 0;
 constexpr GLuint kGlesInvalidShader = 0;
 
@@ -19,6 +22,9 @@ void UnbindBuffer(GLuint target);
 void CopyBuffer(GLuint from, GLuint to,
     GLintptr from_offset, GLintptr to_offset,
     GLsizeiptr size);
+
+
+
 }  // namespace Simple3D::Internal
 
 #endif  // INCLUDE_SIMPLE3D_GRAPHICS_INTERNAL_MISC_H_
