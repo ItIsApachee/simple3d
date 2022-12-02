3831e37abb9076ae915765bf38c194bb7b313f99
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-02 07:21:28 +0300

    Add normal, and texture coordinates argument

--- src/graphics/internal/shader_sources/vertex.glsl
+++ src/graphics/internal/shader_sources/vertex.glsl
@@ -2,13 +2,15 @@
 precision highp float;
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 out vec3 ourColor;
 
-uniform mat4 transform;
+uniform mat4 model;
 
 void main() {
-    gl_Position = transform * vec4(aPos, 1.0);
-    ourColor = aColor;
+    gl_Position = model * vec4(aPos, 1.0);
+    // ourColor = vec3(1.0, 0.5+0.5*(0.1*aNormal.x + 0.2*aNormal.y + 0.4*aNormal.z), 0.0);
+    ourColor = vec3(1.0, aTexCoords.xy);
 }
