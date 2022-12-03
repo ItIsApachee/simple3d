e5373bf97e3d856c63060043300462125bd8ac63
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-04 00:16:21 +0300

    Add input handling

--- include/simple3d/context/window.h
+++ include/simple3d/context/window.h
@@ -6,11 +6,13 @@
 #ifndef INCLUDE_SIMPLE3D_CONTEXT_WINDOW_H_
 #define INCLUDE_SIMPLE3D_CONTEXT_WINDOW_H_
 
+#include <vector>
 #include <memory>
 
 #include <GLFW/glfw3.h>
 
 #include <simple3d/types.h>
+#include <simple3d/context/input.h>
 
 namespace Simple3D {
 
@@ -22,6 +24,10 @@ class Window : public std::enable_shared_from_this<Window> {
 
   ~Window();
 
+  void AddInputHandler(std::shared_ptr<IInputHandler> input_handler);
+  void AddWindowInputHandler(
+    std::shared_ptr<IWindowInputHandler> window_input_handler);
+
   void SwapBuffers();
   void Destroy();  // destroy window, invalidate pointer
 
@@ -30,6 +36,8 @@ class Window : public std::enable_shared_from_this<Window> {
   explicit Window(GLFWwindow* window);
 
   GLFWwindow* window_{nullptr};
+  std::vector<std::shared_ptr<IInputHandler>> input_handlers_{};
+  std::vector<std::shared_ptr<IWindowInputHandler>> window_input_handlers_{};
 };
 
 
--- src/context/window.cc
+++ src/context/window.cc
@@ -1,13 +1,35 @@
 #include <simple3d/context/window.h>
 
 #include <memory>
+#include <unordered_map>
 
 #include <glad/gles2.h>
 #include <GLFW/glfw3.h>
 
+#include <simple3d/context/input.h>
+
 namespace Simple3D {
 
 
+
+// class WindowManager {
+//  public:
+//   static WindowManager& GetInstance() {
+//     static WindowManager window_manager{};
+//     return window_manager;
+//   }
+  
+//   ~WindowManager() = default;
+
+//   std::shared_ptr<Window> GetWindow(GLFWwindow* window) {
+//     return windows_[window];
+//   }
+
+//  private:
+//   WindowManager() = default;
+
+//   std::unordered_map<GLFWwindow*, std::shared_ptr<Window>> windows_{};
+// };
 
 static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -19,12 +41,25 @@ std::shared_ptr<Window> Window::Create(GLFWwindow* window) {
 }
 
 Window::Window(GLFWwindow* window): window_{window} {
+  // TODO(apachee): consider moving window creation here
+  // so that Window follows RAII: creating Window instance
+  // create window resource
+  // TODO(apachee): set callbacks
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 }
 
 Window::~Window() {
   if (window_ != nullptr)
     Destroy();
+}
+
+void Window::AddInputHandler(std::shared_ptr<IInputHandler> input_handler) {
+  input_handlers_.push_back(input_handler);
+}
+
+void Window::AddWindowInputHandler(
+    std::shared_ptr<IWindowInputHandler> window_input_handler) {
+  window_input_handlers_.push_back(window_input_handler);
 }
 
 void Window::SwapBuffers() {
