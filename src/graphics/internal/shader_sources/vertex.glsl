5c3c55e953698301507c7d821fa0ef4afec1e8fa
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-03 22:00:41 +0300

    Add instancing support

--- src/graphics/internal/shader_sources/vertex.glsl
+++ src/graphics/internal/shader_sources/vertex.glsl
@@ -4,13 +4,15 @@ precision highp float;
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in mat4 aModel;
+layout (location = 7) in vec3 aColor;
 
 out vec3 ourColor;
 
-uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
-    gl_Position = model * vec4(aPos, 1.0);
-    // ourColor = vec3(1.0, 0.5+0.5*(0.1*aNormal.x + 0.2*aNormal.y + 0.4*aNormal.z), 0.0);
+    gl_Position = projection * view * aModel * vec4(aPos, 1.0);
     ourColor = vec3(1.0, aTexCoords.xy);
 }
