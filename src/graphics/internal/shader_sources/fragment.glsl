107185805e25aac0efe76199e7ae93cbf5f88d26
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2022-12-31 12:32:13 +0300

    Prepare shaders for lighting

--- src/graphics/internal/shader_sources/fragment.glsl
+++ src/graphics/internal/shader_sources/fragment.glsl
@@ -1,10 +1,11 @@
 #version 310 es
 precision highp float;
 
-in vec3 ourColor;
+in vec3 diffuse_color;
+in vec3 specular_color;
 
-out vec4 FragColor;
+out vec4 frag_color;
 
 void main() {
-    FragColor = vec4(ourColor, 1.0);
+    frag_color = vec4(diffuse_color, 1.0);
 }
--- src/graphics/internal/shader_sources/vertex.glsl
+++ src/graphics/internal/shader_sources/vertex.glsl
@@ -1,19 +1,25 @@
 #version 310 es
 precision highp float;
 
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoords;
-layout (location = 3) in mat4 aModel;
-layout (location = 7) in vec3 aColor;
+layout (location = 0) in vec3 pos_in;
+layout (location = 1) in vec3 normal_in;
+layout (location = 2) in vec2 tex_coords_in;
+layout (location = 3) in mat4 model_in;
 
-out vec3 ourColor;
+layout (location = 7) in vec3 diffuse_color_in;
+layout (location = 8) in vec3 specular_color_in;
 
-// uniform mat4 model_test;
+out vec3 diffuse_color;
+out vec3 specular_color;
+
 uniform mat4 view;
 uniform mat4 projection;
 
+uniform bool diffuse_texture_enabled;
+uniform bool specular_texture_enabled;
+
 void main() {
-    gl_Position = projection * view * aModel * vec4(aPos, 1.0);
-    ourColor = vec3(1.0, aTexCoords.xy);
+    gl_Position = projection * view * model_in * vec4(pos_in, 1.0);
+
+    diffuse_color = vec3(1.0);
 }
