eb595f5def23cb4570d244868875c3b3ef2f2bca
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-04 00:46:45 +0300

    Add temporary lighting

--- src/graphics/internal/shader_sources/fragment.glsl
+++ src/graphics/internal/shader_sources/fragment.glsl
@@ -1,11 +1,40 @@
 #version 310 es
 precision highp float;
 
+struct DirectionalLight {
+    vec3 direction;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 pos;
+in vec3 normal;
 in vec3 diffuse_color;
 in vec3 specular_color;
 
 out vec4 frag_color;
 
+uniform vec3 view_pos;
+// uniform vec3 ambient_light;
+// #define DIRECTIONAL_LIGHTS 1
+// uniform DirectionalLight directional_light[DIRECTIONAL_LIGHTS]
+
 void main() {
-    frag_color = vec4(diffuse_color, 1.0);
+    vec3 ambient_light = vec3(1.0) * 0.3;
+    DirectionalLight directional_light = DirectionalLight(
+        normalize(vec3(0.2, 0.4, -1.0)),
+        vec3(1.0) * 0.5,
+        vec3(1.0) * 0.3
+    );
+
+    vec3 ambient = ambient_light * diffuse_color;
+    vec3 diffuse = directional_light.diffuse * max(dot(normal, -directional_light.direction), 0.0) * diffuse_color;
+
+    vec3 reflect_dir = reflect(directional_light.direction, normal);
+    vec3 view_dir = normalize(view_pos - pos);
+    float specular_mult = pow(max(dot(view_dir, reflect_dir), 0.0), 16.0);
+    vec3 specular = specular_mult * directional_light.specular * specular_color;
+    // vec3 specular = pow(max(dot(view_dir, reflect_dir), 0.0), 16) * 0.5 * directional_light.specular * specular_color;
+
+    frag_color = vec4(ambient + diffuse + specular, 1.0);
 }
--- src/graphics/internal/shader_sources/vertex.glsl
+++ src/graphics/internal/shader_sources/vertex.glsl
@@ -9,6 +9,8 @@ layout (location = 3) in mat4 model_in;
 layout (location = 7) in vec3 diffuse_color_in;
 layout (location = 8) in vec3 specular_color_in;
 
+out vec3 pos;
+out vec3 normal;
 out vec3 diffuse_color;
 out vec3 specular_color;
 
@@ -21,5 +23,11 @@ uniform bool specular_texture_enabled;
 void main() {
     gl_Position = projection * view * model_in * vec4(pos_in, 1.0);
 
+    pos = (model_in * vec4(pos_in, 1.0)).xyz;
+
+    // FIXME: normal is not in model space
+    normal = normal_in;
+
     diffuse_color = vec3(1.0);
+    specular_color = vec3(1.0);
 }
