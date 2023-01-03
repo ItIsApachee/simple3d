1fc1e108dc677aed0942f7ffebcf92691ab64514
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-04 01:26:53 +0300

    Add light.h

---
+++ include/simple3d/graphics/light.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDE_SIMPLE3D_GRAPHICS_LIGHT_H_
+#define INCLUDE_SIMPLE3D_GRAPHICS_LIGHT_H_
+
+#include <glad/gles2.h>
+
+#include <glm/vec3.hpp>
+
+namespace Simple3D {
+
+
+
+struct DirectionalLight {
+  glm::vec3 direction = glm::vec3(0.0f);
+  glm::vec3 diffuse = glm::vec3(0.0f);
+  glm::vec3 specular = glm::vec3(0.0f);
+  GLfloat shininess{16.0f};
+};
+
+
+
+}  // namespace Simple3D
+
+#endif  // INCLUDE_SIMPLE3D_GRAPHICS_LIGHT_H_
