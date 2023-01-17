35a837e514aa171c10da6d47f6a1f955966d58d0
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-18 01:35:04 +0300

    Remove old comments

--- examples/interactive_scene/main.cc
+++ examples/interactive_scene/main.cc
@@ -11,63 +11,6 @@
 #include <simple3d/graphics/models/cuboid.h>
 #include <simple3d/graphics/internal/shader.h>
 #include <simple3d/utils/fps_camera.h>
-// #include <iostream>
-// #include <thread>
-// #include <chrono>
-// #include <vector>
-// #include <cmath>
-// #include <windows.h>
-
-// #define GLAD_GLES2_IMPLEMENTATION
-// #include <glad/gles2.h>
-
-// #include <GLFW/glfw3.h>
-
-// void error_callback(int error, const char* description) {
-//     std::cerr << "glfw error: " << error << " with message \"" << description << "\"" << std::endl;
-// }
-
-// void render(GLFWwindow* window) {
-//     constexpr int FPS_SAMPLES = 200;
-//     auto start = std::chrono::high_resolution_clock::now();
-//     std::vector<decltype(start)> fps_samples(FPS_SAMPLES, start);
-
-//     glfwMakeContextCurrent(window);
-
-//     GladGLES2Context gl;
-//     int version = gladLoadGLES2Context(&gl, glfwGetProcAddress);
-//     if (version == 0) {
-//         std::cerr << "glad: loading gles2 failed" << std::endl;
-//         return;
-//     }
-//     std::cout << "successfuly loaded GLES2 v" << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
-
-//     const float PI = acos(-1.0);
-
-//     // FIXME: this function may only be called from main thread
-//     // glfwGetFramebufferSize(window, &display_w, &display_h);
-//     gl.Viewport(0, 0, 2560, 1440);
-
-//     float v = 0;
-//     int cnt = 0;
-//     // FIXME: should syncronize calls to this function (data race)
-//     while (!glfwWindowShouldClose(window)) {
-//         cnt = (cnt + 1) % FPS_SAMPLES;
-//         auto now = std::chrono::high_resolution_clock::now();
-
-//         int took = (now - fps_samples[cnt]).count();
-//         fps_samples[cnt] = now;
-//         std::cout << "FPS: " << (FPS_SAMPLES / (took / 1e9)) << std::endl;
-
-//         auto elapsed = (now - start).count();
-//         v = elapsed / 1e9;
-
-//         gl.ClearColor((sin(v*2*PI)+1.)/2, 0.0f, 0.0f, 1.0f);
-//         gl.Clear(GL_COLOR_BUFFER_BIT);
-//         // gl.Flush();
-//         glfwSwapBuffers(window);
-//     }
-// }
 
 int main() {
 	using namespace Simple3D;
@@ -100,12 +43,11 @@ int main() {
     for (int i = -v; i <= v; i++) {
         for (int j = -v; j <= v; j++) {
             for (int k = -v; k <= v; k++) {
-                // if (i*i + j*j + k*k <= 31*31)
-                    cubes.push_back(scene.Create<Cuboid>(stride*j, stride*i, stride*k));
+                cubes.push_back(scene.Create<Cuboid>(stride*j, stride*i, stride*k));
             }
         }
         App::PollEvents();
-        std::cout << (i + v) << " / " << (2*v+1) << std::endl;
+        std::cout << (i + v + 1) << " / " << (2*v+1) << std::endl;
     }
     std::cout << "cubes: " << cubes.size() << std::endl;
 
@@ -123,7 +65,6 @@ int main() {
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - prev);
         prev = now;
         cam_handler->Update(elapsed);
-        // angle += 0.01f;
 
         float shift_amp = 0.5;
         float shift = shift_amp * sin(angle);
@@ -135,68 +76,10 @@ int main() {
         }
         angle = millis.count() / 5000.0f;
 
-        // camera->z = dist*cos(angle);
-        // camera->x = dist*sin(angle);
-        // camera->yaw = -angle;
-
 		view.Draw(scene);
         App::SwapBuffers();
 	}
 
     App::Destroy();
-
-
-    // auto init_error = MainLoop::GetInstance().Init();
-    // if (!init_error.IsOk()) {
-    //     std::cerr << "init_error: " << init_error.description << std::endl;
-    //     return -1;
-    // }
-
-    // auto window = WindowBuilder().Title("test").Build();
-    // auto scene = Scene::Create();
-    // scene->SetContext(window);
-
-    // auto loop_error = MainLoop::GetInstance().Start();
-    // if (!loop_error.IsOk()) {
-    //     std::cerr << "loop_error: " << loop_error.description << std::endl;
-    // }
-
-    // MainLoop::GetInstance().Destroy();
-
-
-    // if (!glfwInit()) {
-    //     std::cerr << "glfw initialization failed" << std::endl;
-    //     return 1;
-    // }
-    // glfwSetErrorCallback(error_callback);
-
-    // glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
-
-    // glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-
-    // // doesn't work with ANGLE for some reason
-    // // glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE); // disable vsync
-
-    // GLFWwindow* window = glfwCreateWindow(500, 500, "My title", nullptr, nullptr);
-    // if (!window) {
-    //     std::cerr << "window creation failed" << std::endl;
-    //     return 2;
-    // }
-
-    // glfwSetWindowAspectRatio(window, 16, 9);
-
-    // std::thread render_thread(render, window);
-
-    // while (!glfwWindowShouldClose(window)) {
-    //     glfwWaitEvents();
-    // }
-
-    // render_thread.join();
-
-    // glfwDestroyWindow(window);
-    // glfwTerminate();
-
     return 0;
 }
--- src/graphics/scene.cc
+++ src/graphics/scene.cc
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <memory>
 #include <utility>
-// #include <chrono>
 
 #include <glad/gles2.h>
 #include <glm/glm.hpp>
@@ -13,8 +12,6 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <simple3d/graphics/camera.h>
-// #include <simple3d/shader/shader.h>
-// #include <simple3d/shader/shader_source.h>
 
 namespace Simple3D {
 
@@ -45,125 +42,6 @@ void Scene::RemoveDirectionalLight(const std::shared_ptr<DirectionalLight>& dir_
 void Scene::SetAmbientLight(const glm::vec3& light) {
   ambient_light = light;
 }
-/*
-// void render_scene(std::shared_ptr<Scene> scene) {
-//     GladGLES2Context gl = scene->window_->GetGLES2Context();
-
-//     auto start = std::chrono::high_resolution_clock::now();
-
-//     // float vertices[] = {
-//     //     0.5f,  0.5f, 0.0f,  // top right
-//     //     0.5f, -0.5f, 0.0f,  // bottom right
-//     //     -0.5f, -0.5f, 0.0f,  // bottom left
-//     //     -0.5f,  0.5f, 0.0f   // top left 
-//     // };
-
-//     Error shader_program_error;
-//     GlesShader shader_program = GlesShaderBuilder()
-        .VertexShaderSource(kVertexShader)
-        .FragmentShaderSource(kFragmentShader).Build(
-          &gl, shader_program_error);
-//     if (!shader_program.IsValid() || !shader_program_error.IsOk()) {
-//         std::cerr << "bad shader program: "
-            << shader_program_error.description
-            std::cerr << std::endl;
-//         return;
-//     }
-
-//     float vertices[] = {
-//         // positions         // colors
-//         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-//         -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-//         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-//     };    
-
-//     unsigned int vao = 0;
-//     unsigned int vbo = 0;
-
-//     gl.GenVertexArrays(1, &vao);  
-//     gl.GenBuffers(1, &vbo);
-
-//     gl.BindVertexArray(vao);
-//     gl.BindBuffer(GL_ARRAY_BUFFER, vbo);
-//     gl.BufferData(
-        GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-//     gl.BindBuffer(GL_ARRAY_BUFFER, vbo);
-//     gl.BufferData(
-          GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-//     gl.VertexAttribPointer(
-        0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-//     gl.EnableVertexAttribArray(0);
-//     gl.VertexAttribPointer(
-        1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-//     gl.EnableVertexAttribArray(1);
-//     // gl.VertexAttribPointer(
-       //   0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-//     // gl.EnableVertexAttribArray(0);  
-
-//     // gl.VertexAttribPointer(
-       //   0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-//     // gl.EnableVertexAttribArray(1);  
-
-//     // ..:: Drawing code (in render loop) :: ..
-//     // 4. draw the object
-//     // glUseProgram(shaderProgram);
-//     // glBindVertexArray(VAO);
-//     // someOpenGLFunctionThatDrawsOurTriangle();   
-//     std::vector<int> a = std::vector<int>{1, 2, 3};
-
-//     while (scene->is_running_.test_and_set()) {
-//         auto now = std::chrono::high_resolution_clock::now();
-//         auto elapsed = (now - start).count();
-//         float v = elapsed / (float)1e9 * 10;
-//         v = (float)(sin(v/2.)+1.)/(float)2.0;
-
-//         glm::mat4 transform = glm::mat4(1.0f);
-//         transform = glm::rotate(
-            transform, (float)(v*2.*glm::pi<float>()), glm::vec3(0., 0., 1.));
-//         shader_program.SetUniformMat4fv(&gl, "transform", transform);
-
-//         gl.ClearColor(0., 0., 0., 1.0f);
-//         gl.Clear(GL_COLOR_BUFFER_BIT);
-
-//         // gl.UseProgram(shader_program);
-//         shader_program.Use(&gl);
-//         gl.BindVertexArray(vao);
-//         gl.DrawArrays(GL_TRIANGLES, 0, 3);
-//         // // gl.DrawElements(
-           // GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0); 
-//         gl.BindVertexArray(0);
-
-//         gl.Flush();
-
-//         scene->window_->SwapBuffers();
-//     }
-// }
-
-// Scene::Scene() {
-    // is_running_.test_and_set();
-// }
-
-// void Scene::SetContext(std::shared_ptr<Window> window) {
-//     if (window_) {
-//         is_running_.clear();
-//         if (render_thread_.joinable())
-//             render_thread_.join();
-//     }
-//     window_ = std::move(window);
-//     render_thread_ = std::thread(render_scene, shared_from_this());
-// }
-
-// void Scene::ClearContext() {
-//     if (window_) {
-//         is_running_.clear();
-//         if (render_thread_.joinable())
-//             render_thread_.join();
-//     }
-// }
-*/
 
 
 
