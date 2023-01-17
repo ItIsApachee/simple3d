6f14e40ebc281ff350df76cf0087ccf0fd9fc5b1
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-17 17:45:25 +0300

    Add better ShaderStorage

--- include/simple3d/graphics/shader.h
+++ include/simple3d/graphics/shader.h
@@ -1,8 +1,12 @@
 #ifndef INCLUDE_SIMPLE3D_GRAPHICS_SHADER_H_
 #define INCLUDE_SIMPLE3D_GRAPHICS_SHADER_H_
 
+#include <unordered_map>
+
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
+
+#include <simple3d/types.h>
 
 namespace Simple3D {
 
@@ -20,6 +24,33 @@ class IShader {
 
   // TODO(apahcee): add lighting support
 };
+
+// namespace Internal {
+
+
+
+// template <typename S>
+// class ShaderStorage {
+//  public:
+//   static ShaderStorage& GetInstance() {
+//     static ShaderStorage shader_storage{};
+//     return shader_storage;
+//   }
+
+//   ShaderStorage(const ShaderStorage&) = delete;
+//   ShaderStorage(ShaderStorage&&) = delete;
+//   ShaderStorage& operator=(const ShaderStorage&) = delete;
+//   ShaderStorage& operator=(ShaderStorage&&) = delete;
+
+//   std::unordered_map<Scene*, S> storage{};
+
+//  private:
+//   ShaderStorage() = default;
+// };
+
+
+
+// }
 
 
 
---
+++ include/simple3d/graphics/shader_storage.h
@@ -0,0 +1,90 @@
+#ifndef INCLUDE_SIMPLE3D_GRAPHICS_SHADER_STORAGE_H_
+#define INCLUDE_SIMPLE3D_GRAPHICS_SHADER_STORAGE_H_
+
+#include <unordered_map>
+#include <memory>
+#include <typeinfo>
+#include <typeindex>
+#include <type_traits>
+
+#include <simple3d/misc/error.h>
+#include <simple3d/graphics/shader.h>
+
+namespace Simple3D {
+
+
+
+// definition
+class ShaderStorage {
+ public:
+  ShaderStorage& GetInstance();
+
+  ShaderStorage(const ShaderStorage&) = delete;
+  ShaderStorage(ShaderStorage&&) = delete;
+  ShaderStorage& operator=(const ShaderStorage&) = delete;
+  ShaderStorage& operator=(ShaderStorage&&) = delete;
+  ~ShaderStorage() = default;
+
+  template <typename S, typename... Args>
+  Error InitShader(Args&&... args);
+
+  // if not initialized, use default constructor to initialize
+  template <typename S, std::enable_if_t<std::is_default_constructible_v<S>, bool> = true>
+  std::weak_ptr<IShader> GetShader();
+
+  // if not initialized, return empty weak_ptr
+  template <typename S, std::enable_if_t<!std::is_default_constructible_v<S>, bool> = true>
+  std::weak_ptr<IShader> GetShader();
+
+  void Clear();
+
+ private:
+  ShaderStorage() = default;
+
+  std::unordered_map<std::type_index, std::shared_ptr<IShader>> shaders_{};
+};
+
+
+
+// implementation
+template <typename S, typename... Args>
+Error ShaderStorage::InitShader(Args&&...) {
+  if (auto it = shaders_.find(typeid(S)); it != shaders_.end()) {
+    return Error(ErrorType::kAlreadyInitialized, 
+                 "such shader type is already initialized");
+  }
+
+  auto shader_ptr = std::make_shared<S>();
+
+  // shared_ptr will be destroyed anyways, so move it
+  shaders_[typeid(S)] = std::dynamic_pointer_cast(
+      std::move(shader_ptr));
+}
+
+template <typename S, std::enable_if_t<std::is_default_constructible_v<S>, bool>>
+std::weak_ptr<IShader> ShaderStorage::GetShader() {
+  if (auto it = shaders_.find(typeid(S)); it != shaders_.end()) {
+    return it->second;
+  }
+
+  auto shader_ptr = std::make_shared<S>();
+
+  // store the pointer, and return the weak version of it
+  // shared_ptr will be destroyed anyways, so move it
+  return shaders_[typeid(S)] = std::dynamic_pointer_cast(
+      std::move(shader_ptr));
+}
+
+template <typename S, std::enable_if_t<!std::is_default_constructible_v<S>, bool>>
+std::weak_ptr<IShader> ShaderStorage::GetShader() {
+  if (auto it = shaders_.find(typeid(S)); it != shaders_.end()) {
+    return it->second;
+  }
+  return {};
+}
+
+
+
+}  // namespace Simple3D
+
+#endif  // INCLUDE_SIMPLE3D_GRAPHICS_SHADER_STORAGE_H_
