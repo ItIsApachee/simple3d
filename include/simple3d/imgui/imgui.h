bcf59735af77fce10826d42654b4bc5f6a7e63c2
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-18 17:51:23 +0300

    Add InputHandler

--- include/simple3d/imgui/imgui.h
+++ include/simple3d/imgui/imgui.h
@@ -1,6 +1,11 @@
 #ifndef INCLUDE_SIMPLE3D_IMGUI_IMGUI_H_
 #define INCLUDE_SIMPLE3D_IMGUI_IMGUI_H_
 
+#include <memory>
+#include <unordered_set>
+
+#include <simple3d/context/input.h>
+
 namespace Simple3D::ImGui {
 
 
@@ -10,15 +15,15 @@ void NewFrame();
 void Render();
 void DestroyContext();
 
-class ImGuiContext {
+class Context {
  public:
-  static ImGuiContext& GetInstance();
+  static Context& GetInstance();
 
-  ImGuiContext(const ImGuiContext&) = delete;
-  ImGuiContext(ImGuiContext&&) = delete;
-  ImGuiContext& operator=(const ImGuiContext&) = delete;
-  ImGuiContext& operator=(ImGuiContext&&) = delete;
-  ~ImGuiContext() = default;
+  Context(const Context&) = delete;
+  Context(Context&&) = delete;
+  Context& operator=(const Context&) = delete;
+  Context& operator=(Context&&) = delete;
+  ~Context() = default;
 
   void CreateContext();
   void NewFrame();
@@ -26,7 +31,50 @@ class ImGuiContext {
   void DestroyContext();
 
  private:
-  ImGuiContext() = default;
+  Context() = default;
+};
+
+class InputHandler : public IInputHandler, public IWindowInputHandler {
+ public:
+  InputHandler();
+  InputHandler(const InputHandler&) = delete;
+  InputHandler(InputHandler&&) = default;
+  InputHandler& operator=(const InputHandler&) = delete;
+  InputHandler& operator=(InputHandler&&) = default;
+  ~InputHandler() override = default;
+
+  void ToggleInputs(bool enable);
+
+  void EnableInputHandler(const std::shared_ptr<IInputHandler>&);
+  void EnableWindowInputHandler(const std::shared_ptr<IWindowInputHandler>&);
+  void DisableInputHandler(const std::shared_ptr<IInputHandler>&);
+  void DisableWindowInputHandler(const std::shared_ptr<IWindowInputHandler>&);
+
+  void KeyCallback(int key, int scancode, int action, int mods) override;
+  void CharCallback(unsigned int codepoint) override;
+  void CharModsCallback(unsigned int codepoint, int mods) override;
+  void MouseButtonCallback(int button, int action, int mods) override;
+  void CursorPosCallback(double xpos, double ypos) override;
+  void CursorEnterCallback(int entered) override;
+  void ScrollCallback(double xoffset, double yoffset) override;
+  void DropCallback(int path_count, const char* paths[]) override;
+
+  void WindowPosCallback(int xpos, int ypos) override;
+  void WindowSizeCallback(int width, int height) override;
+  void WindowCloseCallback() override;
+  void WindowRefreshCallback() override;
+  void WindowFocusCallback(int focused) override;
+  void WindowIconifyCallback(int iconified) override;
+  void WindowMaximizeCallback(int maximized) override;
+  void FramebufferSizeCallback(int width, int height) override;
+  void WindowContentScaleCallback(float xscale, float yscale) override;
+
+ private:
+  GLFWwindow* window{nullptr};
+  bool enabled{true};
+
+  std::unordered_set<std::shared_ptr<IInputHandler>> input_handlers_{};
+  std::unordered_set<std::shared_ptr<IWindowInputHandler>> window_input_handlers_{};
 };
 
 
--- src/imgui/imgui.cc
+++ src/imgui/imgui.cc
@@ -1,11 +1,14 @@
 #include <simple3d/imgui/imgui.h>
 
+#include <memory>
+
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
 #include <imgui-gles3/imgui_impl_gles3.h>
 #include <GLFW/glfw3.h>
 
 #include <simple3d/context/context.h>
+#include <simple3d/context/input.h>
 
 // can't call macro in Simple3D::ImGui
 // because of the ImGui part of the namespace
@@ -16,49 +19,203 @@ static void check_version() {
 namespace Simple3D::ImGui {
 
 void CreateContext() {
-  ImGuiContext::GetInstance().CreateContext();
+  Context::GetInstance().CreateContext();
 }
 
 void NewFrame() {
-  ImGuiContext::GetInstance().NewFrame();
+  Context::GetInstance().NewFrame();
 }
 
 void Render() {
-  ImGuiContext::GetInstance().Render();
+  Context::GetInstance().Render();
 }
 
 void DestroyContext() {
-  ImGuiContext::GetInstance().DestroyContext();
+  Context::GetInstance().DestroyContext();
 }
 
-ImGuiContext& ImGuiContext::GetInstance() {
-  static ImGuiContext ctx{};
+Context& Context::GetInstance() {
+  static Context ctx{};
   return ctx;
 }
 
-void ImGuiContext::CreateContext() {
+void Context::CreateContext() {
   ::check_version();
 
   ::ImGui::CreateContext();
   ImGui_ImplGlfw_InitForOpenGL(App::GetGLFWwindow(), false);
+
+  // FIXME: currently simple3d doesn't have monitor callback
+  // and at this moment this allows the following:
+  glfwSetMonitorCallback(ImGui_ImplGlfw_MonitorCallback);
+
   ImGui_ImplGles3_Init();
 }
 
-void ImGuiContext::NewFrame() {
+void Context::NewFrame() {
   ImGui_ImplGles3_NewFrame();
   ImGui_ImplGlfw_NewFrame();
   ::ImGui::NewFrame();
 }
 
-void ImGuiContext::Render() {
+void Context::Render() {
   ::ImGui::Render();
   ImGui_ImplGles3_RenderDrawData(::ImGui::GetDrawData());
 }
 
-void ImGuiContext::DestroyContext() {
+void Context::DestroyContext() {
   ImGui_ImplGles3_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ::ImGui::DestroyContext();
 }
+
+InputHandler::InputHandler() : window{App::GetGLFWwindow()} {}
+
+void InputHandler::ToggleInputs(bool enable) {
+  enabled = enable;
+}
+
+void InputHandler::EnableInputHandler(const std::shared_ptr<IInputHandler>& input_handler) {
+  input_handlers_.insert(input_handler);
+}
+
+void InputHandler::EnableWindowInputHandler(
+    const std::shared_ptr<IWindowInputHandler>& window_input_handler) {
+  window_input_handlers_.insert(window_input_handler);
+}
+
+void InputHandler::DisableInputHandler(const std::shared_ptr<IInputHandler>& input_handler) {
+  input_handlers_.erase(input_handler);
+}
+
+void InputHandler::DisableWindowInputHandler(
+    const std::shared_ptr<IWindowInputHandler>& window_input_handler) {
+  window_input_handlers_.erase(window_input_handler);
+}
+
+
+
+void InputHandler::KeyCallback(int key, int scancode, int action, int mods) {
+  if (enabled) {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+    if (::ImGui::GetIO().WantCaptureKeyboard) {
+      return;
+    }
+  }
+  for (auto& input_handler : input_handlers_) {
+    input_handler->KeyCallback(key, scancode, action, mods);
+  }
+}
+void InputHandler::CharCallback(unsigned int codepoint) {
+  if (enabled) {
+    ImGui_ImplGlfw_CharCallback(window, codepoint);
+    if (::ImGui::GetIO().WantCaptureKeyboard) {
+      return;
+    }
+  }
+  for (auto& input_handler : input_handlers_) {
+    input_handler->CharCallback(codepoint);
+  }
+}
+void InputHandler::CharModsCallback(unsigned int codepoint, int mods) {
+  for (auto& input_handler : input_handlers_) {
+    input_handler->CharModsCallback(codepoint, mods);
+  }
+}
+void InputHandler::MouseButtonCallback(int button, int action, int mods) {
+  if (enabled) {
+    ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
+    if (::ImGui::GetIO().WantCaptureMouse) {
+      return;
+    }
+  }
+  for (auto& input_handler : input_handlers_) {
+    input_handler->MouseButtonCallback(button, action, mods);
+  }
+}
+void InputHandler::CursorPosCallback(double xpos, double ypos) {
+  if (enabled) {
+    ImGui_ImplGlfw_CursorPosCallback(window, xpos, ypos);
+    if (::ImGui::GetIO().WantCaptureMouse) {
+      return;
+    }
+  }
+  for (auto& input_handler : input_handlers_) {
+    input_handler->CursorPosCallback(xpos, ypos);
+  }
+}
+void InputHandler::CursorEnterCallback(int entered) {
+  ImGui_ImplGlfw_CursorEnterCallback(window, entered);
+  for (auto& input_handler : input_handlers_) {
+    input_handler->CursorEnterCallback(entered);
+  }
+}
+void InputHandler::ScrollCallback(double xoffset, double yoffset) {
+  if (enabled) {
+    ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
+    if (::ImGui::GetIO().WantCaptureMouse) {
+      return;
+    }
+  }
+  for (auto& input_handler : input_handlers_) {
+    input_handler->ScrollCallback(xoffset, yoffset);
+  }
+}
+void InputHandler::DropCallback(int path_count, const char* paths[]) {
+  for (auto& input_handler : input_handlers_) {
+    input_handler->DropCallback(path_count, paths);
+  }
+}
+
+
+
+void InputHandler::WindowPosCallback(int xpos, int ypos) {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowPosCallback(xpos, ypos);
+  }
+}
+void InputHandler::WindowSizeCallback(int width, int height) {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowSizeCallback(width, height);
+  }
+}
+void InputHandler::WindowCloseCallback() {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowCloseCallback();
+  }
+}
+void InputHandler::WindowRefreshCallback() {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowRefreshCallback();
+  }
+}
+void InputHandler::WindowFocusCallback(int focused) {
+  ImGui_ImplGlfw_WindowFocusCallback(window, focused);
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowFocusCallback(focused);
+  }
+}
+void InputHandler::WindowIconifyCallback(int iconified) {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowIconifyCallback(iconified);
+  }
+}
+void InputHandler::WindowMaximizeCallback(int maximized) {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowMaximizeCallback(maximized);
+  }
+}
+void InputHandler::FramebufferSizeCallback(int width, int height) {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->FramebufferSizeCallback(width, height);
+  }
+}
+void InputHandler::WindowContentScaleCallback(float xscale, float yscale) {
+  for (auto& window_input_handler : window_input_handlers_) {
+    window_input_handler->WindowContentScaleCallback(xscale, yscale);
+  }
+}
+
+
 
 }  // namespace Simple3D::ImGui
