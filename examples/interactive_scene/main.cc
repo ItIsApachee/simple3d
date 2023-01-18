8663b46e23a9b5440000a414ef77b8848e84ca89
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-18 18:22:00 +0300

    Adapt to new fps cam input handler API

--- examples/interactive_scene/main.cc
+++ examples/interactive_scene/main.cc
@@ -8,6 +8,7 @@
 #include <imgui.h>
 
 #include <simple3d/simple3d.h>
+#include <simple3d/context/input.h>
 #include <simple3d/graphics/model.h>
 #include <simple3d/graphics/camera.h>
 #include <simple3d/graphics/models/cuboid.h>
@@ -15,83 +16,114 @@
 #include <simple3d/utils/fps_camera.h>
 #include <simple3d/imgui/imgui.h>
 
+class FocusFpsCam : public Simple3D::IInputHandler {
+ public:
+  explicit FocusFpsCam(
+      const std::shared_ptr<Simple3D::FpsCameraInputHandler>& v_,
+      const std::shared_ptr<Simple3D::Camera>& c_)
+      : fps_cam_handler{v_}, cam{c_} {}
+    
+  void MouseButtonCallback(int button, int action, int mods) override {
+    if (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS) {
+      if (fps_cam_handler && !fps_cam_handler->IsEnabled()) {
+        fps_cam_handler->Enable(cam);
+      }
+    }
+  }
+ private:
+  std::shared_ptr<Simple3D::FpsCameraInputHandler> fps_cam_handler{};
+  std::shared_ptr<Simple3D::Camera> cam{};
+};
+
 int main() {
 	auto app_init_error = Simple3D::App::Init();
-    if (!app_init_error.IsOk()) {
-        std::cerr << "initialization failed: " << app_init_error.description << std::endl;
-        return -1;
+  if (!app_init_error.IsOk()) {
+    std::cerr << "initialization failed: " << app_init_error.description << std::endl;
+    return -1;
+  }
+
+  Simple3D::ImGui::CreateContext();
+
+  auto imgui_handler = std::make_shared<Simple3D::ImGui::InputHandler>();
+  Simple3D::App::EnableInputHandler(imgui_handler);
+  Simple3D::App::EnableWindowInputHandler(imgui_handler);
+  imgui_handler->ToggleInputs(false);
+
+
+  std::cout << "init succ" << std::endl;
+  
+  Simple3D::View view{};
+  Simple3D::Scene scene{};
+
+  auto camera = std::make_shared<Simple3D::Camera>();
+  scene.SetCamera(camera);
+
+  Simple3D::FpsCameraConfig cfg{};
+  cfg.raw_motion_enabled = true;
+  cfg.escape_key = GLFW_KEY_ESCAPE;
+  auto cam_handler = std::make_shared<Simple3D::FpsCameraInputHandler>(cfg);
+  // Simple3D::App::EnableInputHandler(cam_handler);
+  // Simple3D::App::EnableWindowInputHandler(cam_handler);
+  imgui_handler->EnableInputHandler(cam_handler);
+  imgui_handler->EnableWindowInputHandler(cam_handler);
+  cam_handler->Enable(camera);
+
+  imgui_handler->EnableInputHandler(std::make_shared<FocusFpsCam>(
+    cam_handler, camera));
+
+  std::vector<Simple3D::Model<Simple3D::Cuboid>> cubes;
+  int v = 1;
+  float stride = 1.5f;
+  for (int i = -v; i <= v; i++) {
+    for (int j = -v; j <= v; j++) {
+      for (int k = -v; k <= v; k++) {
+        cubes.push_back(scene.Create<Simple3D::Cuboid>(stride*j, stride*i, stride*k));
+      }
+    }
+    Simple3D::App::PollEvents();
+    std::cout << (i + v + 1) << " / " << (2*v+1) << std::endl;
+  }
+  std::cout << "cubes: " << cubes.size() << std::endl;
+
+  int cnt = 0;
+  float angle = 0.0f;
+  float dist = 10.0f;
+  auto start = std::chrono::high_resolution_clock::now();
+  auto prev = start;
+  while (!Simple3D::App::ShouldClose()) {
+    Simple3D::App::PollEvents();
+
+    auto now = std::chrono::high_resolution_clock::now();
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
+
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - prev);
+    prev = now;
+
+    if (cam_handler->Update(elapsed)) {
+        imgui_handler->ToggleInputs(true);
     }
 
-    Simple3D::ImGui::CreateContext();
-
-    std::cout << "init succ" << std::endl;
-    
-	Simple3D::View view{};
-	Simple3D::Scene scene{};
-
-    auto camera = std::make_shared<Simple3D::Camera>();
-    scene.SetCamera(camera);
-
-    Simple3D::FpsCameraConfig cfg{};
-    cfg.raw_motion_enabled = true;
-    cfg.window = Simple3D::App::GetGLFWwindow();
-    auto cam_handler = std::make_shared<Simple3D::FpsCameraInputHandler>(cfg);
-    Simple3D::App::EnableInputHandler(cam_handler);
-    Simple3D::App::EnableWindowInputHandler(cam_handler);
-    // cam_handler->Enable(camera);
-
-    std::vector<Simple3D::Model<Simple3D::Cuboid>> cubes;
-    int v = 1;
-    float stride = 1.5f;
-    for (int i = -v; i <= v; i++) {
-        for (int j = -v; j <= v; j++) {
-            for (int k = -v; k <= v; k++) {
-                cubes.push_back(scene.Create<Simple3D::Cuboid>(stride*j, stride*i, stride*k));
-            }
+    float shift_amp = 0.5;
+    float shift = shift_amp * sin(angle);
+    for (int i = 9; i < 18; i++) {
+        cubes[i]->y = shift;
+        if (i % 2) {
+            cubes[i]->y = -cubes[i]->y;
         }
-        Simple3D::App::PollEvents();
-        std::cout << (i + v + 1) << " / " << (2*v+1) << std::endl;
     }
-    std::cout << "cubes: " << cubes.size() << std::endl;
+    angle = millis.count() / 5000.0f;
 
-	int cnt = 0;
-    float angle = 0.0f;
-    float dist = 10.0f;
-    auto start = std::chrono::high_resolution_clock::now();
-    auto prev = start;
-	while (!Simple3D::App::ShouldClose()) {
-		Simple3D::App::PollEvents();
+    view.Draw(scene);
 
-        auto now = std::chrono::high_resolution_clock::now();
-        auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
+    Simple3D::ImGui::NewFrame();
+    ImGui::ShowDemoWindow();
+    Simple3D::ImGui::Render();
 
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - prev);
-        prev = now;
-        cam_handler->Update(elapsed);
+    Simple3D::App::SwapBuffers();
+  }
 
-        float shift_amp = 0.5;
-        float shift = shift_amp * sin(angle);
-        for (int i = 9; i < 18; i++) {
-            cubes[i]->y = shift;
-            if (i % 2) {
-                cubes[i]->y = -cubes[i]->y;
-            }
-        }
-        angle = millis.count() / 5000.0f;
+  Simple3D::ImGui::DestroyContext();
+  Simple3D::App::Destroy();
 
-		view.Draw(scene);
-
-        Simple3D::ImGui::NewFrame();
-
-        ImGui::ShowDemoWindow();
-
-        Simple3D::ImGui::Render();
-
-        Simple3D::App::SwapBuffers();
-	}
-
-    Simple3D::ImGui::DestroyContext();
-
-    Simple3D::App::Destroy();
-    return 0;
+  return 0;
 }
