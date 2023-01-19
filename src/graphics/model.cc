015763121fd9c9b5f6bfa31e418f596718ec4046
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-19 12:20:03 +0300

    Fix model texture creation

--- src/graphics/model.cc
+++ src/graphics/model.cc
@@ -85,10 +85,16 @@ class TextureLoader {
       }
 
       glGenTextures(1, &texture.id);
-      glBindTexture(GL_TEXTURE_2D, texture.id);
 
+      glBindTexture(GL_TEXTURE_2D, texture.id);
       glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
           GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
       
       stbi_image_free(data);
 
