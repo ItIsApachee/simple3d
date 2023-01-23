3cb4002a8075b62df37bc0369de5cfcd0583a403
Apachee <22821748+ItIsApachee@users.noreply.github.com>
2023-01-23 04:32:56 +0300

    Add sphere to primitves.h

--- include/simple3d/graphics/models/primitives.h
+++ include/simple3d/graphics/models/primitives.h
@@ -3,9 +3,6 @@
 
 #include <simple3d/types.h>
 #include <simple3d/graphics/models/cuboid.h>
-
-namespace Simple3D {
-
-}  // namespace Simple3D
+#include <simple3d/graphics/models/sphere.h>
 
 #endif  // INCLUDE_SIMPLE3D_GRAPHICS_MODELS_PRIMITIVES_H_
