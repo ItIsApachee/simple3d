18b792c19fa9172682f566ad31d35d705313c0c6
Shakir Bikmetov <shakirbik@gmail.com>
2025-05-01 21:18:59 +0300

    Add namespace-wide int aliases

---
+++ simple3d/core/types.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+namespace NSimple3D {
+
+////////////////////////////////////////////////////////////////////////////////
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+using ui8 = std::uint8_t;
+using ui16 = std::uint16_t;
+using ui32 = std::uint32_t;
+using ui64 = std::uint64_t;
+
+////////////////////////////////////////////////////////////////////////////////
+
+}  // namespace NSimple3D
