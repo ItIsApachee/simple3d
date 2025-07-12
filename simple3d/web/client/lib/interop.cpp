aead2072f1e265945186e4aef7949d9ea9002f0c
Shakir Bikmetov <shakirbik@gmail.com>
2025-07-12 09:36:15 +0300

    Add interop with js page

---
+++ simple3d/web/client/lib/interop.cpp
@@ -0,0 +1,56 @@
+#include "interop.h"
+
+#include <condition_variable>
+#include <iostream>
+
+namespace NSimple3D::NWebClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::mutex InitWebSocketMutex;
+std::condition_variable InitWebSocketCondVar;
+std::optional<std::string> WebSocketUrl;
+
+////////////////////////////////////////////////////////////////////////////////
+
+}  // namespace
+
+////////////////////////////////////////////////////////////////////////////////
+
+void Simple3DInitWebSocket(std::string url)
+{
+    std::cout << std::format("Simple3DInitWebSocket called with url = {}", url) << std::endl;
+
+    {
+        std::lock_guard guard(InitWebSocketMutex);
+        WebSocketUrl = url;
+    }
+    InitWebSocketCondVar.notify_all();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+EMSCRIPTEN_BINDINGS(Simple3DInterop)
+{
+    emscripten::function("Simple3DInitWebSocket", &Simple3DInitWebSocket);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::string GetInitWebSocketOptions()
+{
+    std::unique_lock guard(InitWebSocketMutex);
+    InitWebSocketCondVar.wait(guard, [] {
+        return WebSocketUrl.has_value();
+    });
+
+    return *WebSocketUrl;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+}  // namespace NSimple3D::NWebClient
---
+++ simple3d/web/client/lib/interop.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <emscripten/bind.h>
+
+namespace NSimple3D::NWebClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+void Simple3DInitWebSocket(std::string url);
+
+///////////////////////////////////////////////////////////////////////////////
+
+//! If Simple3DInitWebSocket was already called,
+//! returns websocket url.
+//! Otherwise, blocks execution until it called,
+//! and then proceeds to return websocket url.
+std::string GetInitWebSocketOptions();
+
+////////////////////////////////////////////////////////////////////////////////
+
+}  // namespace NSimple3D::NWebClient
